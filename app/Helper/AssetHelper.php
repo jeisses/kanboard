2859f4fb583da42d2957fd34a6e5e60799046649
Jesse Eisses <jesse@eisses.email>
2025-03-30 17:41:50 +0200

    Fix AssetHelper for absolute paths

--- app/Helper/AssetHelper.php
+++ app/Helper/AssetHelper.php
@@ -4,6 +4,32 @@ namespace Kanboard\Helper;
 
 use Kanboard\Core\Base;
 
+function isAbsolutePath($path) {
+    if (!is_string($path)) {
+        $mess = sprintf('String expected but was given %s', gettype($path));
+        throw new \InvalidArgumentException($mess);
+    }
+    if (!ctype_print($path)) {
+       $mess = 'Path can NOT have non-printable characters or be empty';
+       throw new \DomainException($mess);
+    }
+    // Optional wrapper(s).
+    $regExp = '%^(?<wrappers>(?:[[:print:]]{2,}://)*)';
+    // Optional root prefix.
+    $regExp .= '(?<root>(?:[[:alpha:]]:/|/)?)';
+    // Actual path.
+    $regExp .= '(?<path>(?:[[:print:]]*))$%';
+    $parts = [];
+    if (!preg_match($regExp, $path, $parts)) {
+        $mess = sprintf('Path is NOT valid, was given %s', $path);
+        throw new \DomainException($mess);
+    }
+    if ('' !== $parts['root']) {
+        return true;
+    }
+    return false;
+}
+
 /**
  * Asset Helper
  *
@@ -21,8 +47,13 @@ class AssetHelper extends Base
      */
     public function js($filename, $async = false)
     {
-        $dir = dirname(__DIR__,2);
-        $filepath = $dir.'/'.$filename;
+        if (isAbsolutePath($filename)) {
+            $dir = dirname(__DIR__,2);
+            $filepath = $filename;
+	} else {
+            $filepath = $dir.'/'.$filename;
+        }
+
         return '<script '.($async ? 'async' : '').' defer type="text/javascript" src="'.$this->helper->url->dir().$filename.'?'.filemtime($filepath).'"></script>';
     }
 
